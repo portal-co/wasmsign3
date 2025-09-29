c98fc99291b1cac11372ce715a8b3ce76a4c815b
Graham Kelly <gkgoat6700@gmail.com>
2025-09-29 04:02:20 +0000

    simplify and add payload

--- src/module.rs
+++ src/module.rs
@@ -50,34 +50,35 @@ pub fn put_pad<E>(writer: &mut impl Write<Error = E>, mut value: u64) -> Result<
     }
     Ok(())
 }
-pub fn read_custom_section<E, R: Read<Error = E>>(
-    reader: &mut R,
-    name_hash: Option<[u8; 32]>,
-) -> Result<CustomSection<'_, R>, ReadExactError<E>> {
+pub fn read_custom_section<'a, E, R: Read<Error = E>, T>(
+    reader: &'a mut R,
+    name_hash: &mut (dyn FnMut([u8; 32]) -> Option<T> + '_),
+) -> Result<CustomSection<'a, R, T>, ReadExactError<E>> {
     loop {
         let mut ty = 0u8;
         reader.read_exact(slice::from_mut(&mut ty))?;
         let mut len = get(reader, None)?;
         match ty {
-            0 => match name_hash.as_ref() {
-                None => return Ok(CustomSection { len, reader }),
-                Some(name_hash) => {
-                    let name_len = get(reader, Some(&mut len))?;
-                    let mut hash = sha3::Sha3_256::default();
-                    for _ in 0..name_len {
+            0 => {
+                let name_len = get(reader, Some(&mut len))?;
+                let mut hash = sha3::Sha3_256::default();
+                for _ in 0..name_len {
+                    reader.read_exact(slice::from_mut(&mut ty))?;
+                    hash.update(&[ty]);
+                }
+                let hash: [u8; 32] = hash.finalize().into();
+                if let Some(payload) = name_hash(hash) {
+                    return Ok(CustomSection {
+                        len,
+                        reader,
+                        payload,
+                    });
+                } else {
+                    while len != 0 {
                         reader.read_exact(slice::from_mut(&mut ty))?;
-                        hash.update(&[ty]);
-                    }
-                    let hash: [u8; 32] = hash.finalize().into();
-                    if hash == *name_hash {
-                        return Ok(CustomSection { len, reader });
-                    } else {
-                        while len != 0 {
-                            reader.read_exact(slice::from_mut(&mut ty))?;
-                        }
                     }
                 }
-            },
+            }
             _ => {
                 while len != 0 {
                     reader.read_exact(slice::from_mut(&mut ty))?;
@@ -87,14 +88,15 @@ pub fn read_custom_section<E, R: Read<Error = E>>(
     }
 }
 // #[non_exhaustive]
-pub struct CustomSection<'a, R> {
+pub struct CustomSection<'a, R, T = ()> {
     len: u64,
     reader: &'a mut R,
+    pub payload: T,
 }
-impl<'a, R: Read> ErrorType for CustomSection<'a, R> {
+impl<'a, R: Read, T> ErrorType for CustomSection<'a, R, T> {
     type Error = R::Error;
 }
-impl<'a, R: Read> Read for CustomSection<'a, R> {
+impl<'a, R: Read, T> Read for CustomSection<'a, R, T> {
     fn read(&mut self, mut buf: &mut [u8]) -> Result<usize, Self::Error> {
         if buf.len() as u64 > self.len {
             buf = &mut buf[..(self.len as usize)];
@@ -104,14 +106,14 @@ impl<'a, R: Read> Read for CustomSection<'a, R> {
         return Ok(a);
     }
 }
-impl<'a,R> CustomSection<'a,R>{
-    pub fn len(&self) -> u64{
+impl<'a, R, T> CustomSection<'a, R, T> {
+    pub fn len(&self) -> u64 {
         return self.len;
     }
-    pub fn take(self) -> Result<&'a mut R,Self>{
-        match self.len{
-            0 => Ok(self.reader),
-            _ => Err(self)
+    pub fn take(self) -> Result<(&'a mut R, T), Self> {
+        match self.len {
+            0 => Ok((self.reader, self.payload)),
+            _ => Err(self),
         }
     }
 }
@@ -120,7 +122,7 @@ macro_rules! read_custom_section_with_name {
     ($a:expr, $b:expr) => {
         $crate::module::read_custom_section(
             $a,
-            $crate::__::core::option::Option::Some($crate::__::sha3_literal!($b)),
+            &mut |a|a==$crate::__::sha3_literal!($b),
         )
     };
 }
